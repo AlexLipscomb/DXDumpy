058fe4257eaf57f08e5449c9735362ec1f95d3cd
AlexLipscomb <79888233+AlexLipscomb@users.noreply.github.com>
2021-10-01 00:48:10 -0500

    Update __main__.py

--- dxdumpy/__main__.py
+++ dxdumpy/__main__.py
@@ -7,7 +7,6 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-l", "--list", action="store_true", help="List all of the open ports available")
-    parser.add_argument("-t", "--type", help="Idk lol")
     parser.add_argument("-d", "--dump",  nargs=2, help="Dump the voice data of the synth to a file")
     parser.add_argument("-e", "--export", nargs=2, help="Export the saved voice data to the synth")
 
